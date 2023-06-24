7e818c427cdf374c1fd92917c0ab6c7600d82417
jean <panezrimacjeanpaul@gmail.com>
2023-06-23 22:00:25 -0500

    App Terminada

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart'; 
+import 'package:flutter/widgets.dart';
 
-void main() => runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-  )
-);
-
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
 class HomePage extends StatefulWidget {
-
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -22,48 +18,48 @@ class _HomePageState extends State<HomePage> {
       body: Stack(
         children: <Widget>[
           Container(
-            decoration:  const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/place6.jpg'),
-                fit: BoxFit.cover)
-            ),
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/place6.jpg'),
+                    fit: BoxFit.cover)),
             child: Container(
               decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  colors: [
-                  Colors.black.withOpacity(0.3),
-                  Colors.black.withOpacity(0.3),
-                ])),
-                child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Container(
-                        height: 250,
-                        child: ListView(
-                          scrollDirection: Axis.horizontal,
-                          children: [
-                            makeItem()
-                          ],
-                        ),
+                  gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+                Colors.black.withOpacity(0.3),
+                Colors.black.withOpacity(0.3),
+              ])),
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      height: 250,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          makeItem(image: 'assets/images/place2.jpg'),
+                          makeItem(image: 'assets/images/place6.jpg'),
+                          makeItem(image: 'assets/images/place4.jpg'),
+                        ],
                       ),
-                      SizedBox(
-                        height: 30,
-                      )
-                    ],
-                  ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    )
+                  ],
                 ),
+              ),
             ),
           )
         ],
       ),
     );
   }
-  Widget makeItem({image}){
+
+  Widget makeItem({image}) {
     return AspectRatio(
-      aspectRatio: 1.7/2,
+      aspectRatio: 1.7 / 2,
       child: Container(
         margin: EdgeInsets.only(right: 20),
         padding: EdgeInsets.all(20),
@@ -72,21 +68,53 @@ class _HomePageState extends State<HomePage> {
           color: Colors.white70,
         ),
         child: Column(
+          /* crossAxisAlignment: CrossAxisAlignment.start, */
           children: <Widget>[
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-            Container(
-              width: 60,
-              height: 60,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                image: DecorationImage(
-                  image: AssetImage('assets/images/place5.jpg')
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    image: DecorationImage(
+                        image: AssetImage(image), fit: BoxFit.cover),
+                  ),
+                ),
+                Container(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.grey.shade400),
+                  child: Text(
+                    'Jean',
+                    style: TextStyle(color: Colors.black54),
+                  ),
                 )
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'Texto de Descripcion',
+              style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w400),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Icon(
+                Icons.star_border,
+                color: Colors.blue,
               ),
             )
-            ],
-            ),
           ],
         ),
       ),
